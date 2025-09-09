fbc1a56d58bcc54fa97cf9d77425048a663d8522
ThiagoSezerio123 <sezerio.thiago@escola.pr.gov.br>
2025-09-09 08:46:16 -0300

    k

---
+++ Perguntas.js
@@ -0,0 +1,65 @@
+criaCartao(
+    'Categoria',
+    'pergunta',
+    'resposta'
+)    
+
+criaCartao(
+    'Categoria',
+    'Qual é o menor país do mundo?',
+    'Vaticano'
+)   
+
+criaCartao(
+    'Categoria',
+    'Qual é a capital da Finlândia?',
+    'Helsinque'
+)    
+
+criaCartao(
+    'Categoria',
+    'Qual é o carro mais caro do mundo?',
+    'Rolls-Royce La Rose Noire Droptail'
+)   
+
+criaCartao(
+    'Categoria',
+    'Qual é o maior planeta do sistema solar?',
+    'Júpiter'
+)  
+
+criaCartao(
+    'Categoria',
+    'Qual é o maior trator do mundo?',
+    'Big Bud 747'
+)   
+
+criaCartao(
+    'Categoria',
+    'Em que ano o homem pisou na Lua pela primeira vez?',
+    '20 de julho de 1969'
+)    
+
+criaCartao(
+    'Categoria',
+    '',
+    ''
+)    
+
+criaCartao(
+    'Categoria',
+    '',
+    ''
+)    
+
+criaCartao(
+    'Categoria',
+    '',
+    ''
+)    
+
+criaCartao(
+    'Categoria',
+    '',
+    ''
+)    
